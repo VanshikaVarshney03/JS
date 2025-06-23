66f25ef412ba1bcbd0ce3c1efe42ab4a2c5fddcb
VanshikaVarshney03 <vanshikavarshney0311@gmail.com>
2025-06-23 09:02:06 +0000

    strings and operators

---
+++ Basics/lec7.js
@@ -0,0 +1,38 @@
+//operations
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/3)
+
+
+let str1 ="hello"
+let str2 = "hi"
+let str3 = str1+str2
+console.log(str3)
+
+//if
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);//if string first all will we string
+console.log(1+2+"2")//if string is end then first will we converetd
+
+console.log(+true)//1
+//console(+"")//0
+//prefix postfix
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
