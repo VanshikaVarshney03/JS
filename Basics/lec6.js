f2524c57d6019e55434f620ae781b5323419492a
VanshikaVarshney03 <vanshikavarshney0311@gmail.com>
2025-06-23 07:53:56 +0000

    conversion of datatypes

---
+++ Basics/lec6.js
@@ -0,0 +1,21 @@
+ let score = "33"
+
+
+
+
+ console.log(typeof score);
+ //same as 
+ console.log(typeof (score));
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber);
+ console.log( valueInNumber);
+
+//  "33"=>33
+// "33abs"=>NaN
+// true = 1 ;False = 0
+let isLoggesIn =1
+let booleanLoggedIn = Boolean(isLoggesIn)
+console.log(booleanLoggedIn);
+//1=> true 0 =>false
+//""=>false
+//"hitesh"=.true
