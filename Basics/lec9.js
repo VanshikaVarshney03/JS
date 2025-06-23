a0474032e03f268883a770b92256c1176f1b34fe
VanshikaVarshney03 <vanshikavarshney0311@gmail.com>
2025-06-23 09:18:13 +0000

    datatypes summary

---
+++ Basics/lec9.js
@@ -0,0 +1,24 @@
+ //primitive
+ //7types:String ,Number,null,Boolean,symbol,undefined,BigInt
+
+//JavaScript is a dynamically typed language.
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id==anotherId); //not same even the value passed are same therefore symbol make it unique
+ //Refrence/Ninprimitive
+ //Arrays,Objects,Functions
+
+ const hero = ["hema","ritu"]
+ let myObj = {
+    name:"harshit",
+    age:21,
+ }
+
+  const myfun = function(){
+     console.log("hello"); 
+ }
+
+console.log(typeof myfun);
+// https://262.ecma-international.org/5.1/#sec-11.4.3
