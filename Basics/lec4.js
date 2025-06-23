1e8fed76140bb26996fd760725ff69f62fc12f89
VanshikaVarshney03 <vanshikavarshney0311@gmail.com>
2025-06-23 06:19:50 +0000

    datatypes

---
+++ Basics/lec4.js
@@ -0,0 +1,11 @@
+  const accountId = 144553
+ let accountEmail = "vanshi@google.com"
+ var accountPassword = "12345"
+ accountCity = "Jaipur"
+// accountId =2
+// console.log(accountId) not allowed 
+
+accountEmail="hc@hc.com"
+accountPassword="6799"
+accountCity="banglore"
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ Basics/lec5.js
@@ -0,0 +1,6 @@
+ "use strict";//treat all js code as new version
+// alert(3+3) //we can using nodejs,not browser 
+
+console.log(typeof null);
+console.log(typeof "hitesh")
+console.log(typeof undefined)
